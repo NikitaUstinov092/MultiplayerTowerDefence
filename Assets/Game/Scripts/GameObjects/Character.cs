4be83864f6203cea5743bff1922063e34e7988bc
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2026-09-15 15:45:30 +0300

    Сделал корректное перемещение

---
+++ Assets/Game/Scripts/GameObjects/Character.cs
@@ -0,0 +1,34 @@
+using Fusion;
+using UnityEngine;
+
+namespace SampleGame
+{
+    public sealed class Character : NetworkBehaviour,
+        MoveComponent.ICondition,
+        WeaponComponent.ICondition
+    {
+        [SerializeField]
+        private HealthComponent _healthComponent;
+
+        [SerializeField]
+        private MoveComponent _moveComponent;
+
+        [SerializeField]
+        private WeaponComponent _weaponComponent;
+
+        public override void Spawned()
+        {
+            _moveComponent.SetCondition(this);
+            _weaponComponent.SetCondition(this);
+        }
+        
+        bool MoveComponent.ICondition.IsMet()
+        {
+            if(_healthComponent == null)
+                return true;
+            return _healthComponent.IsAlive;
+        }
+
+        bool WeaponComponent.ICondition.IsMet() => _healthComponent.IsAlive;
+    }
+}
