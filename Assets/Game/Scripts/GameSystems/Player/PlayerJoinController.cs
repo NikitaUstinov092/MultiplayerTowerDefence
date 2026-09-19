40151fc177cd674a7f18268e66977cddb1461aec
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2026-09-19 15:33:58 +0300

    Поправил спавн прифаба

--- Assets/Game/Scripts/GameSystems/Player/PlayerJoinController.cs
+++ Assets/Game/Scripts/GameSystems/Player/PlayerJoinController.cs
@@ -1,4 +1,5 @@
 using Fusion;
+using Game;
 using UnityEngine;
 
 namespace SampleGame
@@ -6,12 +7,14 @@ namespace SampleGame
     public sealed class PlayerJoinController : SimulationBehaviour, IPlayerJoined
     {
         [SerializeField] private GameObject _characterPrefab;
+        [SerializeField] private SpawnPointService _spawnPointService;
 
         void IPlayerJoined.PlayerJoined(PlayerRef player)
         {
             if (this.Runner.IsServer)
             {
-                NetworkObject character = this.Runner.Spawn(_characterPrefab, Vector3.zero, Quaternion.identity, player);
+                Transform spawnPoint = _spawnPointService.GetSpawnPoint(player.AsIndex % _spawnPointService.Count);
+                NetworkObject character = this.Runner.Spawn(_characterPrefab, spawnPoint.position, spawnPoint.rotation, player);
                 this.Runner.SetPlayerObject(player, character); // Index peer, NetworkId
 
                 if (character.TryGetBehaviour(out TeamComponent team))
